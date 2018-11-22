d4e740867be5283b37ce52a7eec8b3cc6d3a7af0
jstjst <jonas@gappert.de>
2018-11-22 17:26:01 +0100

    Added Command Handler to set callsign

--- APRS-Send/APRS-Send.ino
+++ APRS-Send/APRS-Send.ino
@@ -1,35 +1,56 @@
 #include "src/LibAPRS/src/LibAPRS.h"
+#include "src/MLP/CommandHandler.h"
 
 #define ADC_REFERENCE REF_3V3
 #define OPEN_SQUELCH false
+
+#define eeAddress = 0
+
+struct APRS_Settings {
+  char Callsign[6];
+  int ssid;
+};
+
+APRS_Settings aprsSettings = {
+  "NOCALL",
+  1,
+};
+
+CommandHandler<> SerialCommandHandler;
 
 void aprs_msg_callback(struct AX25Msg *msg)
 {}
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(4800);
+
+  SerialCommandHandler.AddCommand(F("SetCall"), SetCall)
+
   APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
   APRS_setCallsign("DC1MAK", 1);
   APRS_printSettings();
-
-  for(int i=0; i<5; i++)
-  {
-    sendLocation;
-  }
 }
 
 void loop()
 {
+  sendLocation();
+  delay(1000);
 }
 
 void sendLocation()
 {
-  APRS_setLat("4822.909N");
-  APRS_setLon("00943.195E");
+  APRS_setLat("4822.90N");
+  APRS_setLon("00943.19E");
 
   char *comment = "LibAPRS Test";
 
   APRS_sendLoc(comment, strlen(comment));
 }
 
+void SetCall(CommandParameter &paramerters)
+{
+  aprsSettings.Callsign = parameters.NextParameter();
+  aprsSettings.ssid = parameters.NextParameterAsInteger(1);
+  APRS_setCallsign(aprsSettings.Callsign, aprsSettings.ssid);
+}
