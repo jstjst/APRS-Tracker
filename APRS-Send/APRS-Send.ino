ccdde354c6321ffc9ba6d95bc03d8e6b289dab7e
jstjst <jonas@gappert.de>
2019-01-27 18:42:41 +0100

    Send Test

--- APRS-Send/APRS-Send.ino
+++ APRS-Send/APRS-Send.ino
@@ -1,41 +1,28 @@
 #include "src/LibAPRS/src/LibAPRS.h"
-#include "src/MLP/CommandHandler.h"
+
 
 #define ADC_REFERENCE REF_3V3
 #define OPEN_SQUELCH false
 
-#define eeAddress = 0
 
-struct APRS_Settings {
-  char Callsign[6];
-  int ssid;
-};
-
-APRS_Settings aprsSettings = {
-  "NOCALL",
-  1,
-};
-
-CommandHandler<> SerialCommandHandler;
 
 void aprs_msg_callback(struct AX25Msg *msg)
 {}
 
 void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(9600);
 
-  SerialCommandHandler.AddCommand(F("SetCall"), SetCall)
 
   APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
-  APRS_setCallsign("DC1MAK", 1);
-  APRS_printSettings();
+  APRS_setCallsign("DN1US", 1);
+  APRS_printSettings(Serial);
 }
 
 void loop()
 {
   sendLocation();
-  delay(1000);
+  delay(10000);
 }
 
 void sendLocation()
@@ -46,11 +33,4 @@ void sendLocation()
   char *comment = "LibAPRS Test";
 
   APRS_sendLoc(comment, strlen(comment));
-}
-
-void SetCall(CommandParameter &paramerters)
-{
-  aprsSettings.Callsign = parameters.NextParameter();
-  aprsSettings.ssid = parameters.NextParameterAsInteger(1);
-  APRS_setCallsign(aprsSettings.Callsign, aprsSettings.ssid);
 }
